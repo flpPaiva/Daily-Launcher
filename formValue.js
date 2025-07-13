a9c234c051a059113defdd124902b74816071a4a
Felipe Paiva <flpcastro.paiva@gmail.com>
2025-07-13 20:43:51 -0300

    Fix comments

--- formValue.js
+++ formValue.js
@@ -1,5 +1,5 @@
 // Deixe a fileName vazio para não importar nenhum arquivo.
-// Obs: Se fileName estiver vazio, os nrNFe, date e vlTotal são obrigatórios
+// Obs: Se fileName estiver vazio, os atributos nrNFe, date e vlTotal são obrigatórios
 export const fileName = "";
 export const priority = "Média";
 export const description = "Refeição";
@@ -9,7 +9,7 @@ export const type = "Meal";
 // export const type = "Mercado";
 
 // Deixe esses campos vazios para usar os dados do PDF.
-// no entanto se quiser preencher manualmente, basta preencher os campos abaixo.
+// No entanto se quiser preencher manualmente ou sobrescrever, eles terão prioridade.
 export const nrNFe = ""; // Exemplo: "123456789"
 export const date = ""; // Exemplo: "31/01/2025"
 export const vlTotal = ""; // Exemplo: "100,00"
